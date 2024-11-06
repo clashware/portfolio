8eac667ca5bed9fc8c6d17e3787f1f196b66bf9f
ELCANET\tahk <kamyar.taher@elca.ch>
2024-11-06 18:25:31 +0100

    remove overflow

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-gradient-to-br from-purple-900 to-black-500 relative overflow-hidden">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-0 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-gradient-to-br from-purple-900 to-black-500 relative overflow-hidden">
       {/* Background geometric elements */}
       <div className="absolute top-0 left-0 w-full h-full opacity-30 pointer-events-none overflow-hidden">
         {/* Floating circles */}
